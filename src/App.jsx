3a33fd2d4c2470bf1cdeef73e5943f1e0b2ba895
sejansorker <sejansorker@gmail.com>
2026-04-29 22:06:18 +0600

    header add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Container from "./components/Container"
+import Header from "./components/Header"
 
 
 function App() {
 
   return (
    <>
-   <Container>
-    sfwfwewe
-   </Container>
+   <Header/>
    </>
   )
 }
