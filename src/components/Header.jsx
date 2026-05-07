beca0c51ba4c1443391cd3ea6c956e61bfa1228c
sejansorker <sejansorker@gmail.com>
2026-05-07 23:18:14 +0600

    nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import Container from './Container'
 
 const Header = () => {
   return (
-    <Container>
+    <div className="bg-teal-500 py-8">
+      <Container>
        <div className="flex ">
         <div className="w-1/3">
         logo
@@ -16,9 +17,13 @@ const Header = () => {
         <li>Contact</li></ul>
         </div>
         <div className="w-1/3">
-        <button>Call</button></div>
+        <div className="flex justify-center">
+          <button>Call</button>
+        </div>
+        </div>
        </div>
     </Container>
+    </div>
   )
 }
 
