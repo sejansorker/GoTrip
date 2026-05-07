71c5b0de83507438395b419beb8aa33d888c3b7b
sejansorker <sejansorker@gmail.com>
2026-05-07 23:11:46 +0600

    com

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,19 @@ import Container from './Container'
 const Header = () => {
   return (
     <Container>
-       <div className="">
+       <div className="flex ">
         <div className="w-1/3">
-        nav
+        logo
         </div>
+        <div className="w-1/3">
+        <ul className='flex gap-x-13'><li>Home</li>
+        <li>About</li>
+        <li>Service</li>
+        <li>Blog</li>
+        <li>Contact</li></ul>
+        </div>
+        <div className="w-1/3">
+        <button>Call</button></div>
        </div>
     </Container>
   )
